6fe164881b527e5e5c29f785bbae009eddea3224
zhuyuqian <yuqian.zhu@thoughtworks.com>
2020-10-18 20:47:13 +0800

    [zhuyuqian]update demo4 to use restful api

--- demo4/src/main/java/com/thoughtworks/capacity/gtb/demo4/CarController.java
+++ demo4/src/main/java/com/thoughtworks/capacity/gtb/demo4/CarController.java
@@ -2,13 +2,7 @@ package com.thoughtworks.capacity.gtb.demo4;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cars")
@@ -21,13 +15,13 @@ public class CarController {
   }
 
   // GET /cars
-  @GetMapping("")
-  public List<Car> getAllCars() {
-    return carService.getAllCars();
+  @GetMapping
+  public List<Car> getAllCars(@RequestParam(name = "color", required = false) String color) {
+    return carService.getCars(color, null);
   }
 
   // POST /cars
-  @PostMapping("")
+  @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void createCar(@RequestBody Car car) {
     carService.createCar(car);
@@ -36,11 +30,6 @@ public class CarController {
   // GET /cars/3
   @GetMapping("/{id}")
   public Car getCarByIdRestful(@PathVariable("id") Integer id) {
-    Car car = carService.getCarById(id);
-    if (car == null) {
-//      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "car not found");
-      throw new CarNotFoundException("car not found");
-    }
-    return car;
+   return carService.getCarById(id);
   }
 }
--- demo4/src/main/java/com/thoughtworks/capacity/gtb/demo4/GlobalExceptionHandler.java
+++ demo4/src/main/java/com/thoughtworks/capacity/gtb/demo4/GlobalExceptionHandler.java
@@ -1,23 +1,18 @@
 package com.thoughtworks.capacity.gtb.demo4;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-//  @ExceptionHandler(CarNotFoundException.class)
-//  @ResponseStatus(HttpStatus.NOT_FOUND)
-//  @ResponseBody
-//  public ErrorResult handle(CarNotFoundException ex) {
-//    return new ErrorResult(ex.getMessage());
-//  }
-
   @ExceptionHandler(CarNotFoundException.class)
-  public ResponseEntity<ErrorResult> handle(CarNotFoundException ex) {
-    ErrorResult errorResult = new ErrorResult(ex.getMessage());
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseBody
+  public ErrorResult handle(CarNotFoundException ex) {
+    return new ErrorResult(ex.getMessage());
   }
 }
