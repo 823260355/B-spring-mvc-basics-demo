151b50b897d5638b0747f9af3fe3f055c737057d
zhuyuqian <yuqian.zhu@thoughtworks.com>
2020-10-18 20:38:28 +0800

    [zhuyuqian]update demo3 to use restful api

--- demo3/src/main/java/com/thoughtworks/capacity/gtb/demo3/CarService.java
+++ demo3/src/main/java/com/thoughtworks/capacity/gtb/demo3/CarService.java
@@ -1,9 +1,10 @@
 package com.thoughtworks.capacity.gtb.demo3;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +17,11 @@ public class CarService {
     carMap.put(2, new Car(2, "Benz", "black"));
   }
 
-  public List<Car> getAllCars() {
-    return new ArrayList<>(carMap.values());
+  public List<Car> getCars(String color, String type) {
+    return carMap.values().stream()
+            .filter(car -> color == null || car.getColor().equals(color))
+            .filter(car -> type == null || car.getType().equals(type))
+            .collect(Collectors.toList());
   }
 
   public void createCar(Car car) {
